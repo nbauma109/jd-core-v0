218d56eca03094cf2e78742b377bdc2b5e927126
nbauma109 <nbauma109@github.com>
2022-12-13 18:06:49 +0100

    added enum switch test

---
+++ src/test/java/jd/core/test/SwitchEnum.java
@@ -0,0 +1,35 @@
+package jd.core.test;
+
+public class SwitchEnum {
+
+    public enum ColourEnum {
+        BLUE, GREEN, RED // TODO FIXME does not work when enum constants are unordered 
+    }
+
+    static class ColourObject {
+        public ColourEnum getType() {
+            return ColourEnum.BLUE;
+        }
+    }
+    public static void main(String[] args) {
+        ColourObject colourObject = new ColourObject();
+        print(colourObject);
+    }
+
+    private static void print(ColourObject colourObject) {
+        switch (colourObject.getType()) {
+            case GREEN:
+                System.out.println("Green");
+                break;
+            case BLUE:
+                System.out.println("Blue");
+                break;
+            case RED:
+                System.out.println("Red");
+                break;
+            default:
+                System.out.println("Default (colour)");
+                break;
+        }
+    }
+}
