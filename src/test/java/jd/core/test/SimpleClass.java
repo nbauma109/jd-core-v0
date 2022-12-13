2b77711429ff13ca68340760d6ca8c05c611752b
nbauma109 <nbauma109@github.com>
2022-12-13 09:38:36 +0100

    SimpleClass extends Throwable

--- src/test/java/jd/core/test/SimpleClass.java
+++ src/test/java/jd/core/test/SimpleClass.java
@@ -1,4 +1,10 @@
 package jd.core.test;
 
-public class SimpleClass {
+public class SimpleClass extends Throwable {
+
+    private static final long serialVersionUID = 1L;
+
+    public SimpleClass() {
+        super("Hello World");
+    }
 }
