fc77afb1ebb067f9529a4467daab265ff2990aec
nbauma109 <nbauma109@github.com>
2022-12-15 11:49:17 +0100

    added tests with novars for jdk 1.2 and1.3

--- src/test/java/jd/core/test/TryCatchFinallyBRTest.java
+++ src/test/java/jd/core/test/TryCatchFinallyBRTest.java
@@ -14,26 +14,43 @@ public class TryCatchFinallyBRTest extends AbstractTestCase {
 
     @Test
     public void testJikes() throws Exception {
-        test("jikes-1.22");
+        test("jikes-1.22", false);
+    }
+    @Test
+    public void testJavac_1_2_2_novar() throws Exception {
+        test("javac-1.2.2", true);
     }
 
     @Test
+    public void testJavac_1_3_1_28() throws Exception {
+        test("javac-1.3.1_28", true);
+    }
+   
+    @Test
     public void testJavac_1_4_2_19() throws Exception {
-        test("javac-1.4.2_19");
+        test("javac-1.4.2_19", false);
     }
 
     @Test
     public void testJavac_17_0_4() throws Exception {
-        test("javac-17.0.4");
+        test("javac-17.0.4", false);
     }
 
-    private void test(String compiler) throws IOException {
-        try (InputStream in = getClass().getResourceAsStream("/try-catch-finally-" + compiler + ".jar")) {
+    private void test(String compiler, boolean noVar) throws IOException {
+        StringBuilder expectedFileName = new StringBuilder("TryCatchFinallyBR");
+        StringBuilder jarPath = new StringBuilder("/try-catch-finally-");
+        jarPath.append(compiler);
+        if (noVar) {
+            expectedFileName.append("-novar");
+            jarPath.append("-novar");
+        }
+        expectedFileName.append(".txt");
+        jarPath.append(".jar");
+        try (InputStream in = getClass().getResourceAsStream(jarPath.toString())) {
             ZipLoader loader = new ZipLoader(in);
             String internalClassName = "jd/core/test/TryCatchFinallyBR";
             String output = decompile(internalClassName, loader);
-            assertEquals(IOUtils.toString(getClass().getResource("TryCatchFinallyBR.txt"), StandardCharsets.UTF_8), output);
+            assertEquals(IOUtils.toString(getClass().getResource(expectedFileName.toString()), StandardCharsets.UTF_8), output);
         }
     }
-
 }
