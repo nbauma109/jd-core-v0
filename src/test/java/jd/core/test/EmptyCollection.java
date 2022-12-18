aa55c9f2dca3d72118e286b9b87e89e979deefa1
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-18 14:36:50 +0100

    Bug fix for static final empty map (#53)
    * bug fix for static final empty map
    
    * added singleton map test ("key", "value")
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

---
+++ src/test/java/jd/core/test/EmptyCollection.java
@@ -0,0 +1,9 @@
+package jd.core.test;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class EmptyCollection {
+    static final Map<String, String> EMPTY_MAP = Collections.emptyMap();
+    static final Map<String, String> SINGLETON_MAP = Collections.singletonMap("key", "value");
+}
