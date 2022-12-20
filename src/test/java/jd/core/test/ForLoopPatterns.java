79c66f6d98941482d70edfcb0bf2b681eccb5819
nbauma109 <nbauma109@github.com>
2022-12-20 11:14:01 +0100

    unit test cases for loop patterns

---
+++ src/test/java/jd/core/test/ForLoopPatterns.java
@@ -0,0 +1,43 @@
+package jd.core.test;
+
+public class ForLoopPatterns {
+    private int i;
+
+    // for (beforeLoop; ;)
+    void beforeLoop() {
+        int i = 0;
+        for (;;) {
+            System.out.println(i++);
+            if (i < 10) {
+                continue;
+            }
+        }
+    }
+
+    // for (; ; lastBodyLoop)
+    void lastBodyLoop() {
+        for (;; i++) {
+            System.out.println("start");
+            System.out.println(i);
+            System.out.println("end");
+        }
+    }
+
+    // for (beforeLoop; ; lastBodyLoop)
+    void beforeLooplastBodyLoop() {
+        for (int i = 0;; i++) {
+            System.out.println("start");
+            System.out.println(i);
+            System.out.println("end");
+        }
+    }
+
+    // for (; test; lastBodyLoop)
+    void testlastBodyLoop() {
+        for (; i < 5; i++) {
+            System.out.println(i);
+        }
+        for (; i < 10; i++) {
+        }
+    }
+}
