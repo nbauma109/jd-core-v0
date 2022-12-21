38036198a3fe70d784740effb6b66c8611f78eec
nbauma109 <nbauma109@github.com>
2022-12-21 07:38:22 +0100

    added PrimitiveTypesTest

---
+++ src/test/java/jd/core/test/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package jd.core.test;
+
+public class PrimitiveTypes {
+    Class<Double> getDoubleType() { return double.class; };
+    Class<Long> getLongType() { return long.class; };
+    Class<Float> getFloatType() { return float.class; };
+    Class<Integer> getIntType() { return int.class; };
+    Class<Short> getShortType() { return short.class; };
+    Class<Character> getCharType() { return char.class; };
+    Class<Byte> getByteType() { return byte.class; };
+    Class<Boolean> getBooleanType() { return boolean.class; };
+    Class<Void> getVoidType() { return void.class; };
+}
