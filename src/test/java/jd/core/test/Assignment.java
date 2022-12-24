63521c4b0e8bdbc939a81cfb0c626b325949fdfa
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-24 15:25:30 +0100

    Assignment reconstructor test (#80)

---
+++ src/test/java/jd/core/test/Assignment.java
@@ -0,0 +1,23 @@
+package jd.core.test;
+
+public class Assignment {
+
+    static int i, j;
+
+    void add(int x) {
+        i = i + x;
+    }
+
+    void add(byte x) {
+        i =
+        i + x;
+    }
+
+    void add(short x) {
+        i = j + x;
+    }
+
+    void add(int x, int y) {
+        i = y + x;
+    }
+}
