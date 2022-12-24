944d6b5ddee912875a6e7985342f51b4684e052d
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-24 16:30:43 +0100

    Update Assignment tests (#82)

--- src/test/java/jd/core/test/Assignment.java
+++ src/test/java/jd/core/test/Assignment.java
@@ -1,8 +1,11 @@
 package jd.core.test;
 
+import java.awt.Point;
+
 public class Assignment {
 
     static int i, j;
+    int a, b, c;
 
     void add(int x) {
         i = i + x;
@@ -19,5 +22,21 @@ public class Assignment {
 
     void add(int x, int y) {
         i = y + x;
+    }
+
+    static void add (Point p, int x) {
+        p.x += x;
+    }
+
+    // Assignation multiple sur deux lignes :
+    // b = c;
+    // a = b;
+    void multiAssign() {
+        b = c;
+        a = b;
+    }
+
+    void multiAssignInc() {
+        a = b += c;
     }
 }
