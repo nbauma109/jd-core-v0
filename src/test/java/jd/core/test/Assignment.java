007c9fbcafcbfe5abd854da88494a61f2545945e
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-24 15:17:05 +0100

    Update Assignment.java

--- src/test/java/jd/core/test/Assignment.java
+++ src/test/java/jd/core/test/Assignment.java
@@ -2,9 +2,22 @@ package jd.core.test;
 
 public class Assignment {
 
-    static int i;
+    static int i, j;
 
     void add(int x) {
         i = i + x;
+    }
+
+    void add(byte x) {
+        i =
+        i + x;
+    }
+
+    void add(short x) {
+        i = j + x;
+    }
+
+    void add(int x, int y) {
+        i = y + x;
     }
 }
