cbb68305820723322a1fdac9170060169abffe79
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-24 08:09:00 +0100

    Create ByteCodeInput.java

---
+++ src/test/java/jd/core/test/ByteCodeInput.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class ByteCodeInput {
+
+    private Object[][] array;
+
+    public void invokeInterfaceIInc(List<?> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+         }
+    }
+
+    public void multiANewArray() {
+        array = new Object[1][2];
+    }
+}
