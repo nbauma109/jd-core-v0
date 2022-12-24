8974ac5ac23209d609a7e0d4f63481dc5a5cdfa0
nbauma109 <nbauma109@users.noreply.github.com>
2022-12-24 16:22:05 +0100

    Update Assignment.java

--- src/test/java/jd/core/test/Assignment.java
+++ src/test/java/jd/core/test/Assignment.java
@@ -3,6 +3,7 @@ package jd.core.test;
 public class Assignment {
 
     static int i, j;
+    int a, b, c;
 
     void add(int x) {
         i = i + x;
@@ -19,5 +20,21 @@ public class Assignment {
 
     void add(int x, int y) {
         i = y + x;
+    }
+
+    static void add (Point p, int x) {
+        p.x += x;
+    }
+
+    // Assignation multiple sur deux lignes :
+    // b = c;
+    // a = b;
+    void multiAssign() {
+        b = c;
+        a = b;
+    }
+
+    void multiAssignInc() {
+        a = b += c;
     }
 }
