e31c43e903443e9c604e1cf0d00c326912e33957
nbauma109 <nbauma109@github.com>
2023-01-01 21:38:39 +0100

    added dup tests

---
+++ src/test/java/jd/core/test/Dup2Pop2.java
@@ -0,0 +1,32 @@
+package jd.core.test;
+
+public class Dup2Pop2 {
+
+    long l1;
+    long l2;
+    double[] darr;
+
+    void dup2x1Form1(String[] s) {
+        s[0] += "s";
+    }
+
+    void dup2x1Form2() {
+        l2 = l1 = 1;
+    }
+
+    long dup2Form1(long a) {
+        return ++a;
+    }
+
+    void dup2Form2(double d) {
+        darr[0] += d;
+    }
+
+    long dup2x2(long[] array, int i, long l) {
+        return array[i] = l;
+    }
+
+    void pop2() {
+        Math.round(0.5d);
+    }
+}
