ce8119979b109b76f4a577703472ea255a59702f
nbauma109 <nbauma109@github.com>
2023-01-01 15:43:10 +0100

    fix test ForEachReconstruct

--- src/test/java/jd/core/test/ForEachReconstruct.java
+++ src/test/java/jd/core/test/ForEachReconstruct.java
@@ -1,12 +1,23 @@
 package jd.core.test;
 
 public class ForEachReconstruct {
-    void iterate() {
-        String[] strings;
-        int j = (strings = new String[] { "a", "b" }).length; for (int i = 0; i < j; ++i) { String s = strings[i]; System.out.println(s);
+
+    /*
+     * SUN 1.5 pattern
+     */
+    void forEachInCatch() {
+        try {
+            System.out.println("try");
+        } catch (Exception e) {
+            for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+                System.out.println(stackTraceElement);
+            }
         }
     }
 
+    /*
+     * IBM pattern
+     */
     public static void main(String[] args) {
         Object[] localObject = args; int guiMap = 0; for (int localGUIMap1 = localObject.length; guiMap < localGUIMap1; ++guiMap) { Object arg = localObject[guiMap];
             System.out.println(arg);
