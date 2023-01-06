9a91741ad10aac11e9cd4fe5006ab46b996c85a3
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-06 21:03:55 +0100

    bug fix for intersection type signature (#114)
    Co-authored-by: nbauma109 <nbauma109@github.com>

---
+++ src/test/java/jd/core/test/Generic.java
@@ -0,0 +1,13 @@
+package jd.core.test;
+
+import java.io.Externalizable;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.function.Consumer;
+
+public interface Generic {
+    <T extends Serializable, X extends Runnable, Y extends Comparable<T>> void process(T obj, X x, Y y);
+    <T extends Serializable & Runnable, X extends Comparable<T> & Cloneable & Externalizable> void process(T obj);
+    <E> boolean addAll(Collection<? extends E> c);
+    <E> void forEach(Consumer<? super E> action);
+}
