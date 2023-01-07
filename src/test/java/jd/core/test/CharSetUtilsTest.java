dafdc309cd997dea653b72d008d4b5beb25da501
nbauma109 <nbauma109@github.com>
2023-01-07 15:24:35 +0100

    added CharSetUtilsTest

---
+++ src/test/java/jd/core/test/CharSetUtilsTest.java
@@ -0,0 +1,19 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Test for FastInstructionListBuilder.createBreakAndContinue().
+ */
+public class CharSetUtilsTest extends AbstractTestCase {
+    @Test
+    public void test() throws Exception {
+        String output = decompile("org/apache/commons/lang3/CharSetUtils");
+        assertEquals(IOUtils.toString(getClass().getResource("CharSetUtils.txt"), StandardCharsets.UTF_8), output);
+    }
+}
