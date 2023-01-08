98e4b79b0fc2d64bbbbb98d4194e6f6839e031e7
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-08 21:13:27 +0100

    bug fix try/resources - CompareInstructionVisitor (#125)
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/test/java/jd/core/test/TryResources.java
+++ src/test/java/jd/core/test/TryResources.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TryResources {
 
     List<String> readFile(Path path) {
         List<String> lines = new ArrayList<>();
-        try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8); 
-                Scanner sc = new Scanner(br)) {
+        try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8); Scanner sc = new Scanner(br)) {
             while (sc.hasNextLine()) {
                 lines.add(sc.nextLine());
             }
@@ -21,5 +26,21 @@ public class TryResources {
             e.printStackTrace();
         }
         return lines;
+    }
+
+    public static Map.Entry<Integer, String> get(Connection connection, int employeeId) throws SQLException {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM employee WHERE employee_id = ?")) {
+            ps.setInt(1, employeeId);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String employeeName = rs.getString("employee_name");
+                    return new SimpleEntry<>(employeeId, employeeName);
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            throw new SQLException(e);
+        }
+        return null;
     }
 }
