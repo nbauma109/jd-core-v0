08b7daaf567f5bace7aa1c6a026700b31cbce9ce
nbauma109 <nbauma109@github.com>
2023-01-11 19:25:39 +0100

    added SwitchClosureTest

---
+++ src/test/java/jd/core/test/SwitchClosureTest.java
@@ -0,0 +1,16 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class SwitchClosureTest extends AbstractTestCase {
+    @Test
+    public void test() throws Exception {
+        String output = decompile("org/apache/commons/collections4/functors/SwitchClosure");
+        assertEquals(IOUtils.toString(getClass().getResource("SwitchClosure.txt"), StandardCharsets.UTF_8), output);
+    }
+}
