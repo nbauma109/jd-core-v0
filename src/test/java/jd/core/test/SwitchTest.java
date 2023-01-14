a9bb8b5c22333ed8a28176cf8c6750b02446adcb
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-14 19:14:10 +0100

    added methodTrySwitchFinally (#135)
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/test/java/jd/core/test/SwitchTest.java
+++ src/test/java/jd/core/test/SwitchTest.java
@@ -1,8 +1,10 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
@@ -10,7 +12,10 @@ import static org.junit.Assert.assertEquals;
 public class SwitchTest extends AbstractTestCase {
     @Test
     public void test() throws Exception {
-        String output = decompile("jd/core/test/Switch");
-        assertEquals(IOUtils.toString(getClass().getResource("Switch.txt"), StandardCharsets.UTF_8), output);
+        try (InputStream in = getClass().getResourceAsStream("/switch-jdk1.4.2_19.jar")) {
+            ZipLoader loader = new ZipLoader(in);
+            String output = decompile("jd/core/test/Switch", loader);
+            assertEquals(IOUtils.toString(getClass().getResource("Switch.txt"), StandardCharsets.UTF_8), output);
+        }
     }
 }
