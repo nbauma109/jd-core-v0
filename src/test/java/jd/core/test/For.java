705b38aff78ea1f1dc01b3e9038240ec7b1cead9
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-14 18:01:15 +0100

    added ForTest (#133)
    Co-authored-by: nbauma109 <nbauma109@github.com>

---
+++ src/test/java/jd/core/test/For.java
@@ -0,0 +1,12 @@
+package jd.core.test;
+
+public class For {
+
+    public void emptyForTestUpdate(int i) {
+        System.out.println("start");
+
+        for (; i<10; i++);
+
+        System.out.println("end");
+    }
+}
