6ce01d05d5514ce0b5523c81bd25fcec2668669d
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-14 08:29:25 +0100

    update OuterGetStaticTest (#129)

--- src/test/java/jd/core/test/OuterGetStatic.java
+++ src/test/java/jd/core/test/OuterGetStatic.java
@@ -1,48 +1,56 @@
 package jd.core.test;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class OuterGetStatic {
 
     static int[] a;
     static int x, y;
-    static Number d;
-    static Object o;
 
     class Inner {
 
         int[] newInitArray = { x, y };
 
+        void athrow() throws Exception {
+            throw new Exception("Cannot convert " + Object.class + " to " + Void.class);
+        }
+
         int arrayLength() {
-            return a.length;
+            return new Class[] { Float.class }.length;
         }
 
         void arrayStore() {
-            a[x] = y;
+            a[Double.class.hashCode()] = Float.class.hashCode();
         }
 
         int unaryOp() {
-            return -x;
+            return -Double.class.hashCode();
+        }
+
+        void instanceOf() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+            if (newInstance(Object.class) instanceof String) {
+                System.out.println("ok");
+            }
+        }
+        
+        Object newInstance(Class c) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+            return c.getConstructor(new Class[0]).newInstance(new Object[0]);
         }
 
         int binaryOp() {
-            return x + y;
+            return Float.class.hashCode() + Double.class.hashCode();
         }
 
-        int ternaryOp() {
+        int preIncPostInc() {
             return x > 0 ? ++y : x++;
         }
         
         byte convert() {
-            return (byte) x;
-        }
-
-        void instanceOf() throws Exception {
-            if (d instanceof Double) {
-                System.out.println("ok");
-            }
+            return (byte) a[Byte.class.hashCode()];
         }
 
         void tableSwitch() {
-            switch (x) {
+            switch (Object.class.hashCode()) {
                 case 1:
                     System.out.println("One");
                     break;
@@ -58,7 +66,7 @@ public class OuterGetStatic {
         }
 
         void lookupSwitch() {
-            switch (x) {
+            switch (Object.class.hashCode()) {
                 case 1:
                     System.out.println("One");
                     break;
@@ -73,22 +81,22 @@ public class OuterGetStatic {
             }
         }
 
-        void fastSynchronized() {
-            synchronized (o) {
-                System.out.println("ok");
-            }
-        }
-
         Object[] aNewArray() {
-            return new Object[x];
+            return new Object[Object.class.hashCode()];
         }
 
         int[] newArray() {
-            return new int[x];
+            return new int[Object.class.hashCode()];
         }
 
         Object[][] multiANewArray() {
-            return new Object[x][y];
+            return new Object[Object.class.hashCode()][Object.class.hashCode()];
+        }
+        
+        void dupStore() {
+            synchronized (Object.class.toString()) {
+                System.out.println("ok");
+            }
         }
     }
 }
--- src/test/java/jd/core/test/OuterGetStaticTest.java
+++ src/test/java/jd/core/test/OuterGetStaticTest.java
@@ -1,13 +1,25 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
 public class OuterGetStaticTest extends AbstractTestCase {
+    @Test
+    public void testJDK142() throws Exception {
+        try (InputStream in = getClass().getResourceAsStream("/outer-getstatic-jdk1.4.2_19.jar")) {
+            ZipLoader loader = new ZipLoader(in);
+            String internalClassName = "jd/core/test/OuterGetStatic";
+            String output = decompile(internalClassName, loader);
+            assertEquals(IOUtils.toString(getClass().getResource("OuterGetStatic.txt"), StandardCharsets.UTF_8), output);
+        }
+    }
+
     @Test
     public void test() throws Exception {
         String output = decompile("jd/core/test/OuterGetStatic");
