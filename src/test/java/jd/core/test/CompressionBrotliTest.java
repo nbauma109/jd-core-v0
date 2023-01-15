2405d81389ee6e5b0172e41d2668136e08b5bc4f
nbauma109 <nbauma109@github.com>
2023-01-15 17:14:35 +0100

    fixed CompressionBrotliTest

--- src/test/java/jd/core/test/CompressionBrotliTest.java
+++ src/test/java/jd/core/test/CompressionBrotliTest.java
@@ -1,8 +1,11 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.api.loader.Loader;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
@@ -10,7 +13,10 @@ import static org.junit.Assert.assertEquals;
 public class CompressionBrotliTest extends AbstractTestCase {
     @Test
     public void test() throws Exception {
-        String output = decompile("jd/core/test/CompressionBrotli");
-        assertEquals(IOUtils.toString(getClass().getResource("CompressionBrotli.txt"), StandardCharsets.UTF_8), output);
+        try (InputStream in = getClass().getResourceAsStream("/try-resources-brotli-jdk1.8.0_331.jar")) {
+            Loader loader = new ZipLoader(in);
+            String output = decompile("jd/core/test/CompressionBrotli", loader);
+            assertEquals(IOUtils.toString(getClass().getResource("CompressionBrotli.txt"), StandardCharsets.UTF_8), output);
+        }
     }
 }
