f82df539e8bf0fea85c68389215f34f44aac8014
nbauma109 <nbauma109@users.noreply.github.com>
2023-01-22 17:14:29 +0100

    updated TryResources (#140)
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/test/java/jd/core/test/TryResources.java
+++ src/test/java/jd/core/test/TryResources.java
@@ -1,6 +1,9 @@
 package jd.core.test;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,6 +31,20 @@ public class TryResources {
         return lines;
     }
 
+    List<String> readFile(File file) {
+        List<String> lines = new ArrayList<>();
+        try (InputStream in = Files.newInputStream(file.toPath());
+                InputStreamReader isr = new InputStreamReader(in);
+                Scanner sc = new Scanner(isr)) {
+            while (sc.hasNextLine()) {
+                lines.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+    
     public static Map.Entry<Integer, String> get(Connection connection, int employeeId) throws SQLException {
         try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM employee WHERE employee_id = ?")) {
             ps.setInt(1, employeeId);
