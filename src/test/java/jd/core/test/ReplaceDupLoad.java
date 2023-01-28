035f4e7e3fd5885d69b66bcdbb98155811214512
nbauma109 <nbauma109@github.com>
2023-01-28 17:38:25 +0100

    test for ReplaceDupLoadVisitor FOR

--- src/test/java/jd/core/test/ReplaceDupLoad.java
+++ src/test/java/jd/core/test/ReplaceDupLoad.java
@@ -2,6 +2,12 @@ package jd.core.test;
 
 import org.apache.commons.lang3.StringUtils;
 
+import h.ST_pointnlink_t;
+import h.ST_triangle_t;
+import smetana.core.CArray;
+import smetana.core.Z;
+import smetana.core.__ptr__;
+
 public abstract class ReplaceDupLoad {
 
     /*
@@ -19,4 +25,24 @@ public abstract class ReplaceDupLoad {
         return str.substring(0, startOffset) + middle + str.substring(endOffset);
     }
 
+    /*
+     * FOR
+     */
+    public static void loadtriangle(__ptr__ pnlap, __ptr__ pnlbp, __ptr__ pnlcp) {
+        CArray<ST_triangle_t> trip;
+        trip = Z.z().tris.plus_(Z.z().tril++);
+        trip.get__(0).mark = 0;
+        trip.get__(0).e[0].pnl0p = (ST_pointnlink_t) pnlap;
+        trip.get__(0).e[0].pnl1p = (ST_pointnlink_t) pnlbp;
+        trip.get__(0).e[0].rtp = null;
+        trip.get__(0).e[1].pnl0p = (ST_pointnlink_t) pnlbp;
+        trip.get__(0).e[1].pnl1p = (ST_pointnlink_t) pnlcp;
+        trip.get__(0).e[1].rtp = null;
+        trip.get__(0).e[2].pnl0p = (ST_pointnlink_t) pnlcp;
+        trip.get__(0).e[2].pnl1p = (ST_pointnlink_t) pnlap;
+        trip.get__(0).e[2].rtp = null;
+        for (int ei = 0; ei < 3; ei++) {
+            trip.get__(0).e[ei].lrp = trip;
+        }
+    }
 }
