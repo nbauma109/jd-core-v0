d6b9d3a012c91e1330011b1cd689cbd4c2b55ab0
nbauma109 <nbauma109@github.com>
2023-02-12 21:11:58 +0100

    added jd.core.test.PathUtilsTest.testJDK7()

--- src/test/java/jd/core/test/PathUtilsTest.java
+++ src/test/java/jd/core/test/PathUtilsTest.java
@@ -1,8 +1,10 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
@@ -12,5 +14,14 @@ public class PathUtilsTest extends AbstractTestCase {
     public void test() throws Exception {
         String output = decompile("org/apache/commons/io/file/PathUtils");
         assertEquals(IOUtils.toString(getClass().getResource("PathUtils.txt"), StandardCharsets.UTF_8), output);
+    }
+
+    @Test
+    public void testJDK7() throws Exception {
+        try (InputStream in = getClass().getResourceAsStream("/try-resources-jdk1.7.0_80.jar")) {
+            ZipLoader loader = new ZipLoader(in);
+            String output = decompile("jd/core/test/PathUtils", loader);
+            assertEquals(IOUtils.toString(getClass().getResource("PathUtilsJDK7.txt"), StandardCharsets.UTF_8), output);
+        }
     }
 }
