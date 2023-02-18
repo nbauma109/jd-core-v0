f0169ba546f809da43c7972968ef2503d37f4931
nbauma109 <nbauma109@github.com>
2023-02-18 17:09:37 +0100

    added missing test

--- src/test/java/jd/core/test/NameClashTest.java
+++ src/test/java/jd/core/test/NameClashTest.java
@@ -6,11 +6,21 @@ import org.junit.Test;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import jd.core.model.reference.ReferenceMap;
 
 public class NameClashTest extends AbstractTestCase {
+
     @Test
     public void test() throws Exception {
         String output = decompile("jd/core/test/NameClash");
         assertEquals(IOUtils.toString(getClass().getResource("NameClash.txt"), StandardCharsets.UTF_8), output);
+    }
+
+    @Test
+    public void testContainsSimpleNameReurnsFalse() throws Exception {
+        ReferenceMap map = new ReferenceMap();
+        assertFalse(map.containsSimpleName(""));
     }
 }
