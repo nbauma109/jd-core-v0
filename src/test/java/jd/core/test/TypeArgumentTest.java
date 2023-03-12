aa64838cfef8c8d1031fa907f8ed991216f63f2e
nbauma109 <nbauma109@github.com>
2023-03-12 12:32:53 +0100

    fix unit test for JDK8

--- src/test/java/jd/core/test/TypeArgumentTest.java
+++ src/test/java/jd/core/test/TypeArgumentTest.java
@@ -1,8 +1,11 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.api.loader.Loader;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
@@ -10,8 +13,10 @@ import static org.junit.Assert.assertEquals;
 public class TypeArgumentTest extends AbstractTestCase {
     @Test
     public void testTypeArgument() throws Exception {
-        String internalClassName = "jd/core/test/TypeArgument";
-        String output = decompile(internalClassName);
-        assertEquals(IOUtils.toString(getClass().getResource("TypeArgument.txt"), StandardCharsets.UTF_8), output);
+        try (InputStream in = getClass().getResourceAsStream("/type-arguments-jdk1.8.0_331.jar")) {
+            Loader loader = new ZipLoader(in);
+            String output = decompile("jd/core/test/TypeArgument", loader);
+            assertEquals(IOUtils.toString(getClass().getResource("TypeArgument.txt"), StandardCharsets.UTF_8), output);
+        }
     }
 }
