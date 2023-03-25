ea562941face6f06ee262d87e79c0b5a77483aa3
nbauma109 <nbauma109@github.com>
2023-03-25 21:59:34 +0100

    added OverrideAnnotationTest

---
+++ src/test/java/jd/core/test/OverrideAnnotationTest.java
@@ -0,0 +1,16 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class OverrideAnnotationTest extends AbstractTestCase {
+    @Test
+    public void test() throws Exception {
+        String output = decompile("jd/core/test/OverrideAnnotation");
+        assertEquals(IOUtils.toString(getClass().getResource("OverrideAnnotation.txt"), StandardCharsets.UTF_8), output);
+    }
+}
