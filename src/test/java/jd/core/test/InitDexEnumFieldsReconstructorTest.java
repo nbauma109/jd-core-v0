6dec22ed419ba1283e75ac594b4a2435b24d38cd
nbauma109 <nbauma109@github.com>
2023-03-26 20:07:03 +0200

    Revert "StaticTools: only enum is good"
    This reverts commit e733050f7711688d4fa583e9cca0606bf7fd9e44.

--- src/test/java/jd/core/test/InitDexEnumFieldsReconstructorTest.java
+++ src/test/java/jd/core/test/InitDexEnumFieldsReconstructorTest.java
@@ -15,7 +15,7 @@ public class InitDexEnumFieldsReconstructorTest extends AbstractTestCase {
     public void test() throws Exception {
         try (InputStream in = getClass().getResourceAsStream("/fuelprices-dex2jar.jar")) {
             ZipLoader loader = new ZipLoader(in);
-            String internalClassName = "gr/androiddev/FuelPrices/StaticTools$LocationProvider";
+            String internalClassName = "gr/androiddev/FuelPrices/StaticTools";
             String output = decompile(internalClassName, loader);
             assertEquals(IOUtils.toString(getClass().getResource("StaticTools.txt"), StandardCharsets.UTF_8), output);
         }
