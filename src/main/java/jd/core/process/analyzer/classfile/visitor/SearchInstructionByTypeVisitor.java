c14d2a28c3f047d5b65fe8352b04cb7910eb14e8
nbauma109 <nbauma109@github.com>
2023-04-10 21:37:32 +0200

    fix copyright notice

--- src/main/java/jd/core/process/analyzer/classfile/visitor/SearchInstructionByTypeVisitor.java
+++ src/main/java/jd/core/process/analyzer/classfile/visitor/SearchInstructionByTypeVisitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2007-2019 Emmanuel Dupuy GPLv3
+ * Copyright (C) 2007-2023 Emmanuel Dupuy GPLv3 and other contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
