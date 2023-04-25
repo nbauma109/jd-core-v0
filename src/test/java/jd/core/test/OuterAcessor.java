9ef4d7786a1a91b76428222d9321ab6c990c5972
nbauma109 <nbauma109@github.com>
2023-04-25 21:03:20 +0200

    added test for SourceWriterVisitor.needAPrefixForThisField(...)

--- src/test/java/jd/core/test/OuterAcessor.java
+++ src/test/java/jd/core/test/OuterAcessor.java
@@ -2,16 +2,24 @@ package jd.core.test;
 
 public class OuterAcessor {
 
+    int i;
+    
     void init() {
     }
 
     class Inner {
 
+        int i;
+
         void init() {
             new Thread() {
+                int i;
+
                 @Override
                 public void run() {
                     OuterAcessor.this.init();
+                    int i = 0;
+                    this.i = i;
                 }
             }.start();
         }
