47e24ee46049146dc968ec7062fcc214098d22be
nbauma109 <nbauma109@github.com>
2023-05-01 11:30:10 +0200

    fix for empty enum bug

---
+++ src/test/java/jd/core/test/JasicTest.java
@@ -0,0 +1,17 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class JasicTest extends AbstractTestCase {
+
+    @Test
+    public void test() throws Exception {
+        String output = decompile("net/sourceforge/plantuml/jasic/Jasic");
+        assertEquals(IOUtils.toString(getClass().getResource("Jasic.txt"), StandardCharsets.UTF_8), output);
+    }
+}
