4f6bb85a84905c22febed96e090b75eefdd76c4a
nbauma109 <nbauma109@users.noreply.github.com>
2023-05-05 22:17:54 +0200

    Empty foreach and auto-unboxing foreach (#267)
    * Empty foreach and auto-unboxing foreach
    
    * added other primitives
    
    * use StringUtils.equalsAny(...)
    
    ---------
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/test/java/jd/core/test/ForEachReconstruct.java
+++ src/test/java/jd/core/test/ForEachReconstruct.java
@@ -1,5 +1,12 @@
 package jd.core.test;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class ForEachReconstruct {
 
     /*
@@ -22,5 +29,121 @@ public class ForEachReconstruct {
         Object[] localObject = args; int guiMap = 0; for (int localGUIMap1 = localObject.length; guiMap < localGUIMap1; ++guiMap) { Object arg = localObject[guiMap];
             System.out.println(arg);
         }
+    }
+
+    /*
+     * Foreach with auto-unboxing
+     */
+    void enhancedForEachStatement(Integer... ints) {
+        for (Integer anInt : ints) {
+            System.out.println(anInt);
+        }
+        List<Integer> intList = new ArrayList<>();
+        if (ints != null) {
+            for (int theInt : ints) {
+                intList.add(theInt);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Long... longs) {
+        for (Long aLong : longs) {
+            System.out.println(aLong);
+        }
+        List<Long> longList = new ArrayList<>();
+        if (longs != null) {
+            for (long theLong : longs) {
+                longList.add(theLong);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Double... doubles) {
+        for (Double aDouble : doubles) {
+            System.out.println(aDouble);
+        }
+        List<Double> doubleList = new ArrayList<>();
+        if (doubles != null) {
+            for (double theDouble : doubles) {
+                doubleList.add(theDouble);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Float... floats) {
+        for (Float aFloat : floats) {
+            System.out.println(aFloat);
+        }
+        List<Float> floatList = new ArrayList<>();
+        if (floats != null) {
+            for (float theFloat : floats) {
+                floatList.add(theFloat);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Byte... bytes) {
+        for (Byte aByte : bytes) {
+            System.out.println(aByte);
+        }
+        List<Byte> byteList = new ArrayList<>();
+        if (bytes != null) {
+            for (byte theByte : bytes) {
+                byteList.add(theByte);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Short... shorts) {
+        for (Short aShort : shorts) {
+            System.out.println(aShort);
+        }
+        List<Short> shortList = new ArrayList<>();
+        if (shorts != null) {
+            for (short theShort : shorts) {
+                shortList.add(theShort);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Character... chars) {
+        for (Character aChar : chars) {
+            System.out.println(aChar);
+        }
+        List<Character> charList = new ArrayList<>();
+        if (chars != null) {
+            for (char theChar : chars) {
+                charList.add(theChar);
+            }
+        }
+    }
+    
+    void enhancedForEachStatement(Boolean... booleans) {
+        for (Boolean aBoolean : booleans) {
+            System.out.println(aBoolean);
+        }
+        List<Boolean> booleanList = new ArrayList<>();
+        if (booleans != null) {
+            for (boolean theBoolean : booleans) {
+                booleanList.add(theBoolean);
+            }
+        }
+    }
+
+    /*
+     * Empty foreach
+     */
+    void emptyForEach(Class c) {
+        Method[] methods = c.getMethods();
+        Map m = new HashMap();
+        for (Method method : methods) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(method.getName());
+            if (method.getReturnType().equals(void.class)) {
+                sb.append("");
+            }
+            m.put(method.getName(), sb.toString());
+        }
+        for (Iterator iter = m.values().iterator(); iter.hasNext(););
     }
 }
