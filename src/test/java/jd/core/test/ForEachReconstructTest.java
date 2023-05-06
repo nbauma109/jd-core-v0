5633120be969ffa6893016314d8a15ec71f27aa5
nbauma109 <nbauma109@github.com>
2023-05-06 10:56:38 +0200

    added bug fix and test for jdk8 array foreach

--- src/test/java/jd/core/test/ForEachReconstructTest.java
+++ src/test/java/jd/core/test/ForEachReconstructTest.java
@@ -1,9 +1,11 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
@@ -14,5 +16,14 @@ public class ForEachReconstructTest extends AbstractTestCase {
     public void test() throws IOException {
         String output = decompile("jd/core/test/ForEachReconstruct");
         assertEquals(IOUtils.toString(getClass().getResource("ForEachReconstruct.txt"), StandardCharsets.UTF_8), output);
+    }
+    
+    @Test
+    public void test180() throws IOException {
+        try (InputStream in = getClass().getResourceAsStream("/foreach-jdk1.8.0_331.jar")) {
+            ZipLoader loader = new ZipLoader(in);
+            String output = decompile("jd/core/test/ForEachReconstruct", loader);
+            assertEquals(IOUtils.toString(getClass().getResource("ForEachReconstruct.txt"), StandardCharsets.UTF_8), output);
+        }
     }
 }
