6cecc5a7774e826b0e6293d87bb41173d1109113
nbauma109 <nbauma109@github.com>
2023-05-07 18:21:57 +0200

    added TextBlockCucaJSon

---
+++ src/test/java/jd/core/test/TestInnerClassTest.java
@@ -0,0 +1,17 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestInnerClassTest extends AbstractTestCase {
+    @Test
+    public void test() throws Exception {
+        String internalClassName = "jd/core/test/TestInnerClass";
+        String output = decompile(internalClassName);
+        assertEquals(IOUtils.toString(getClass().getResource("TestInnerClass.txt"), StandardCharsets.UTF_8), output);
+    }
+}
