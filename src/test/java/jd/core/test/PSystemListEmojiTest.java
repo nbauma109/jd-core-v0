c7711d001b185c28cdb1696431c4847e505eff96
nbauma109 <nbauma109@users.noreply.github.com>
2023-05-07 18:45:33 +0200

    Replace multiple outer reference test (#277)
    * added StaticBucketMapTest
    
    * added TextBlockCucaJSon
    
    * added TestInnerClass
    
    * added PSystemListEmojiTest
    
    * added MJPEGGeneratorTest
    
    * added ParticipantBoxTest
    
    * added TileParallelTest
    
    ---------
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

---
+++ src/test/java/jd/core/test/PSystemListEmojiTest.java
@@ -0,0 +1,17 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class PSystemListEmojiTest extends AbstractTestCase {
+
+    @Test
+    public void test() throws Exception {
+        String output = decompile("net/sourceforge/plantuml/emoji/PSystemListEmoji");
+        assertEquals(IOUtils.toString(getClass().getResource("PSystemListEmoji.txt"), StandardCharsets.UTF_8), output);
+    }
+}
