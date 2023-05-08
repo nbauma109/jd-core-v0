1a8e1aad19d64e0695947b3d74fda9f64d02793b
nbauma109 <nbauma109@github.com>
2023-05-08 21:53:45 +0200

    added tests for DUP2_X2

---
+++ src/test/java/jd/core/test/Dup2X2Example.java
@@ -0,0 +1,32 @@
+package jd.core.test;
+
+public abstract class Dup2X2Example {
+
+    protected abstract void form1Result(int paramInt1, int paramInt2, int paramInt3, int paramInt4, int paramInt5, int paramInt6);
+    
+    protected abstract void form2Result(long paramLong1, int paramInt1, int paramInt2, long paramLong2);
+    
+    protected abstract void form3Result(int paramInt1, int paramInt2, long paramLong, int paramInt3, int paramInt4);
+    
+    protected abstract void form4Result(long paramLong1, long paramLong2, long paramLong3);
+    
+    // ..., value4, value3, value2, value1 → ..., value2, value1, value4, value3, value2, value1
+    public void form1(int value4, int value3, int value2, int value1) {
+        form1Result(value2, value1, value4, value3, value2, value1);
+    }
+
+    // ..., value3, value2, value1 → ..., value1, value3, value2, value1
+    public void form2(int value3, int value2, long value1) {
+        form2Result(value1, value3, value2, value1);
+    }
+
+    // ..., value3, value2, value1 → ..., value2, value1, value3, value2, value1
+    public void form3(long value3, int value2, int value1) {
+        form3Result(value2, value1, value3, value2, value1);
+    }
+
+    // ..., value2, value1 → ..., value1, value2, value1
+    public void form4(long value2, long value1) {
+        form4Result(value1, value2, value1);
+    }
+}
---
+++ src/test/java/jd/core/test/Dup2X2ExampleCreatorTest.java
@@ -0,0 +1,62 @@
+package jd.core.test;
+
+import org.apache.commons.io.IOUtils;
+import org.jd.core.v1.api.loader.Loader;
+import org.jd.core.v1.loader.ClassPathLoader;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class Dup2X2ExampleCreatorTest extends AbstractTestCase {
+
+    @Test
+    public void test() throws Exception {
+        Dup2X2ExampleCreator creator = new Dup2X2ExampleCreator();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        creator.create(out);
+        Loader loader = new ClassPathLoader() {
+            
+            @Override
+            public byte[] load(String internalName) throws IOException {
+                return "jd/core/test/Dup2X2Example.class".equals(internalName) ? out.toByteArray() : super.load(internalName);
+            }
+            
+            @Override
+            public boolean canLoad(String internalName) {
+                return "jd/core/test/Dup2X2Example.class".equals(internalName) || super.canLoad(internalName);
+            }
+        };
+        String output = decompile("jd/core/test/Dup2X2Example", loader);
+        assertEquals(IOUtils.toString(getClass().getResource("Dup2X2Example.txt"), StandardCharsets.UTF_8), output);
+    }
+
+    @Test
+    public void testDouble() throws Exception {
+        Dup2X2DoubleExampleCreator creator = new Dup2X2DoubleExampleCreator();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        creator.create(out);
+        Loader loader = new ClassPathLoader() {
+            
+            @Override
+            public byte[] load(String internalName) throws IOException {
+                return "jd/core/test/Dup2X2Example.class".equals(internalName) ? out.toByteArray() : super.load(internalName);
+            }
+            
+            @Override
+            public boolean canLoad(String internalName) {
+                return "jd/core/test/Dup2X2Example.class".equals(internalName) || super.canLoad(internalName);
+            }
+        };
+        String output = decompile("jd/core/test/Dup2X2Example", loader);
+        assertEquals(IOUtils.toString(getClass().getResource("Dup2X2DoubleExample.txt"), StandardCharsets.UTF_8), output);
+    }
+
+    @Override
+    protected boolean recompile() {
+        return false;
+    }
+}
