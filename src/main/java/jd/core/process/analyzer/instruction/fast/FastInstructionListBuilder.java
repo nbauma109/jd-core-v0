a9217e9f950bade352bc4049931482dbd9103c89
nbauma109 <nbauma109@users.noreply.github.com>
2023-05-09 21:13:48 +0200

    bug fix and test for CHECKCAST in foreach (#290)
    * bug fix and test for CHECKCAST in foreach
    
    * added 2nd checkcast test
    
    ---------
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/main/java/jd/core/process/analyzer/instruction/fast/FastInstructionListBuilder.java
+++ src/main/java/jd/core/process/analyzer/instruction/fast/FastInstructionListBuilder.java
@@ -3224,6 +3224,9 @@ public final class FastInstructionListBuilder {
             }
             valueref = iv.getObjectref();
         }
+        if (valueref instanceof CheckCast cc) {
+            valueref = cc.getObjectref();
+        }
         if (valueref.getOpcode() != ByteCodeConstants.ARRAYLOAD) {
             return 0;
         }
@@ -3456,6 +3459,9 @@ public final class FastInstructionListBuilder {
             return 0;
         }
         StoreInstruction si = (StoreInstruction) beforeForInstruction;
+        if (si.getValueref() instanceof CheckCast cc) {
+            si.setValueref(cc.getObjectref());
+        }
         if (si.getValueref().getOpcode() == Const.ARRAYLENGTH) {
             ArrayLength al = (ArrayLength) si.getValueref();
             if (al.getArrayref().getOpcode() == ByteCodeConstants.ASSIGNMENT) {
