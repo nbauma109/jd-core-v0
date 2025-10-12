811e91c73697bd06a2640adee7db0b7a15932dd8
nbauma109 <nbauma109@github.com>
2025-10-12 17:54:35 +0200

    fix unit tests

--- src/test/java/jd/core/test/MemoizerTest.java
+++ src/test/java/jd/core/test/MemoizerTest.java
@@ -1,7 +1,6 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
-import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -16,7 +15,7 @@ public class MemoizerTest extends AbstractTestCase {
     @Test
     public void test() throws Exception {
     	try (InputStream in = getClass().getResourceAsStream("/commons-lang3-3.12.0.jar")) {
-    		ZipLoader loader = new ZipLoader(in);
+    		CompositeLoader loader = new CompositeLoader(in);
 	        String output = decompile("org/apache/commons/lang3/concurrent/Memoizer", loader);
 	        assertEquals(IOUtils.toString(getClass().getResource("Memoizer.txt"), StandardCharsets.UTF_8), output);
     	}
