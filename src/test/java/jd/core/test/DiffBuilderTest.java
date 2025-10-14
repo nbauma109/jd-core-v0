44dedc0dcf785bbdbae3c5e7fbd777a1d67a8574
nbauma109 <nbauma109@users.noreply.github.com>
2025-10-14 08:32:11 +0200

    Update DiffBuilderTest.java: use CompositeLoader

--- src/test/java/jd/core/test/DiffBuilderTest.java
+++ src/test/java/jd/core/test/DiffBuilderTest.java
@@ -1,7 +1,6 @@
 package jd.core.test;
 
 import org.apache.commons.io.IOUtils;
-import org.jd.core.v1.util.ZipLoader;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -14,7 +13,7 @@ public class DiffBuilderTest extends AbstractTestCase {
     @Test
     public void test() throws Exception {
     	try (InputStream in = getClass().getResourceAsStream("/commons-lang3-3.12.0.jar")) {
-    		ZipLoader loader = new ZipLoader(in);
+    		CompositeLoader loader = new CompositeLoader(in);
 	        String output = decompile("org/apache/commons/lang3/builder/DiffBuilder", loader);
 	        assertEquals(IOUtils.toString(getClass().getResource("DiffBuilder.txt"), StandardCharsets.UTF_8), output);
     	}
